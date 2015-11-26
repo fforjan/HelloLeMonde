6788b1af005e1edb87a01a4d21c0c5346e67e610
Frederic Forjan <GeoVah@freds-MacBook-Pro.local>
2015-11-25 20:06:03 -0800

    Simple

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Globalization;
+
+namespace HelloLeMonde
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello fred");
+            Console.Read();
+        }
+        
+        public static void Random()
+        {
+            Console.WriteLine("BLA");
+        }
+    }
+}
